29767b62aa61e56e736edb7f6b124d95e38e5ecd
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2024-09-09 16:52:26 +0500

    upadated code

--- front-end/src/Component/Wheel/Wheel.js
+++ front-end/src/Component/Wheel/Wheel.js
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from "react";
-import "../Wheel/Wheel.css";
+import React, { useRef, useState } from 'react';
+import '../Wheel/Wheel.css';
 
 function Wheel() {
   const wheelRef = useRef(null);
@@ -23,41 +23,41 @@ function Wheel() {
 
   return (
     <>
-      <div className="h-[50vh] w-full my-5 flex justify-center items-center">
-        <div className="container">
-          <div className="spinbtn" onClick={handleSpin}>
+      <div className='h-[42vh] w-full my-5 flex justify-center items-center'>
+        <div className='container'>
+          <div className='spinbtn' onClick={handleSpin}>
             Spin
           </div>
-          <div className="wheel" ref={wheelRef}>
-            <span style={{ "--i": 0 }}></span>
-            <span style={{ "--i": 1 }}></span>
-            <span style={{ "--i": 2 }}></span>
-            <span style={{ "--i": 3 }}></span>
-            <div className="number">
-              <b style={{ "--i": 0 }}>5</b>
-              <b style={{ "--i": 1 }}>3</b>
-              <b style={{ "--i": 2 }}>8</b>
-              <b style={{ "--i": 3 }}>9</b>
-              <b style={{ "--i": 4 }}>5</b>
-              <b style={{ "--i": 5 }}>2</b>
-              <b style={{ "--i": 6 }}>1</b>
-              <b style={{ "--i": 7 }}>8</b>
+          <div className='wheel' ref={wheelRef}>
+            <span style={{ '--i': 0 }}></span>
+            <span style={{ '--i': 1 }}></span>
+            <span style={{ '--i': 2 }}></span>
+            <span style={{ '--i': 3 }}></span>
+            <div className='number'>
+              <b style={{ '--i': 0 }}>5</b>
+              <b style={{ '--i': 1 }}>3</b>
+              <b style={{ '--i': 2 }}>8</b>
+              <b style={{ '--i': 3 }}>9</b>
+              <b style={{ '--i': 4 }}>5</b>
+              <b style={{ '--i': 5 }}>2</b>
+              <b style={{ '--i': 6 }}>1</b>
+              <b style={{ '--i': 7 }}>8</b>
             </div>
           </div>
-          <div className="wheel inner" ref={innerRef}>
-            <span style={{ "--i": 0 }}></span>
-            <span style={{ "--i": 1 }}></span>
-            <span style={{ "--i": 2 }}></span>
-            <span style={{ "--i": 3 }}></span>
-            <div className="number">
-              <b style={{ "--i": 0 }}>5</b>
-              <b style={{ "--i": 1 }}>3</b>
-              <b style={{ "--i": 2 }}>8</b>
-              <b style={{ "--i": 3 }}>9</b>
-              <b style={{ "--i": 4 }}>5</b>
-              <b style={{ "--i": 5 }}>2</b>
-              <b style={{ "--i": 6 }}>1</b>
-              <b style={{ "--i": 7 }}>8</b>
+          <div className='wheel inner' ref={innerRef}>
+            <span style={{ '--i': 0 }}></span>
+            <span style={{ '--i': 1 }}></span>
+            <span style={{ '--i': 2 }}></span>
+            <span style={{ '--i': 3 }}></span>
+            <div className='number'>
+              <b style={{ '--i': 0 }}>5</b>
+              <b style={{ '--i': 1 }}>3</b>
+              <b style={{ '--i': 2 }}>8</b>
+              <b style={{ '--i': 3 }}>9</b>
+              <b style={{ '--i': 4 }}>5</b>
+              <b style={{ '--i': 5 }}>2</b>
+              <b style={{ '--i': 6 }}>1</b>
+              <b style={{ '--i': 7 }}>8</b>
             </div>
           </div>
         </div>
--- front-end/src/Component/main.jsx
+++ front-end/src/Component/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import DailyRewardContextProvider from "./components/context/DailyRewardContextProvider.jsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import DailyRewardContextProvider from './components/context/DailyRewardContextProvider.jsx';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <DailyRewardContextProvider>
       <App />
